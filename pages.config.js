81b6f64b38e69385e74bb4664369e1231878070b
ligoushi <184078718@qq.com>
2021-10-25 01:25:43 +0800

    initial commit

---
+++ pages.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  build: {
+    src: 'src',
+    dist: 'release',
+    temp: '.tmp',
+    public: 'public',
+    paths: {
+      styles: 'assets/styles/*.scss',
+      scripts: 'assets/scripts/*.js',
+      pages: '*.html',
+      images: 'assets/images/**',
+      fonts: 'assets/fonts/**'
+    }
+  },
+  data: {
+    menus: [
+      {
+        name: 'Home',
+        icon: 'aperture',
+        link: 'index.html'
+      },
+      {
+        name: 'Features',
+        link: 'features.html'
+      },
+      {
+        name: 'About',
+        link: 'about.html'
+      },
+      {
+        name: 'Contact',
+        link: '#',
+        children: [
+          {
+            name: 'Twitter',
+            link: 'https://twitter.com/ligoushi'
+          },
+          {
+            name: 'About',
+            link: 'https://weibo.com/aaa'
+          },
+          {
+            name: 'divider'
+          },
+          {
+            name: 'About',
+            link: 'https://github.com/ligoushi'
+          }
+        ]
+      }
+    ],
+    pkg: require('./package.json'),
+    date: new Date()
+  }
+}
